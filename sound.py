79fb9c66eb677782a6b8465c28f25351744b98ff
Saurya Jha <sauryajha9841@gmail.com>
2024-05-25 18:06:35 +0545

    ..

--- sound.py
+++ sound.py
@@ -1,10 +1,10 @@
 from gtts import gTTS
 import os
 
-def speak(text):
-    tts = gTTS(text=text, lang='en', slow=False)
+def speak(text, lang='en'):
+    tts = gTTS(text=text, lang=lang, slow=False)
     tts.save("temp.mp3")
     os.system("mpg321 temp.mp3")
 
 # Example usage
-speak("Hello World")
+speak("", lang='fr')  # Speaking in French
